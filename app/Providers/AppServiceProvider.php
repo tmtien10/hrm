6e2cb3eefd85b0329c457d1be23be7416bacfe80
tmtien10 <tmtien10@users.noreply.github.com>
2019-02-25 21:54:41 +0700

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,6 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    use URL;
     public function boot()
     {
         //
