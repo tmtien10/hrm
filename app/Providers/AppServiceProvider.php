3ed83f38d45f8ef2480a731745326820a2ec1c42
tmtien10 <tmtien10@users.noreply.github.com>
2019-02-26 11:47:42 +0700

    commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use URL;
-
 class AppServiceProvider extends ServiceProvider
 {
     /**
@@ -15,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-           URL::forceScheme('https');
+         URL::forceScheme('https');
     }
 
     /**
